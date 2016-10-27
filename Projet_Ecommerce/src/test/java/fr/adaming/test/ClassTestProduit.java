91969821203170ed37550262ee3e9b0ceac79694
adelinep <adeline.pelloin@gmail.com>
2016-10-27 10:07:07 +0200

    classe test

---
+++ Projet_Ecommerce/src/test/java/fr/adaming/test/ClassTestProduit.java
@@ -0,0 +1,18 @@
+package fr.adaming.test;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class ClassTestProduit {
+
+	public static void main(String[] args) {
+
+		ApplicationContext cxt = new FileSystemXmlApplicationContext("C:\\Users\\inti0288\\git\\TP_ECommerceSpring\\Projet_Ecommerce\\src\\main\\webapp\\WEB-INF\\application-context.xml");
+		
+	
+		
+		
+	}
+
+}
